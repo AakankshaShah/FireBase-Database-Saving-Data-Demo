a37dfd671b1fb21b09a526e79857196e087d974d
AKSHASHA <aakankshah18@gmail.com>
2020-07-10 21:01:12 +0530

    FireBaseDatabase

---
+++ app/src/main/java/com/example/firebasedemo3/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.firebasedemo3;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    FirebaseDatabase database;
+    EditText t1,t2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        database = FirebaseDatabase.getInstance();
+        t1=findViewById(R.id.editTextTextPersonName);
+        t2=findViewById(R.id.editTextTextPersonName2);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void clicked(View view)
+    {//Method 1
+        String s1,s2;
+        s1=t1.getText().toString();
+        s2=t2.getText().toString();
+
+        DatabaseReference myRef = database.getReference(s1);
+        myRef.setValue(s2);
+    //Method 2
+
+    }
+}
